781bbc6acc19641e717e82cab84bd283124015bb
gracehorvath <gracehorvath@cpe-172-100-1-251.twcny.res.rr.com>
2016-09-06 21:07:20 -0500

    Styling/Content Updates

--- script.js
+++ script.js
@@ -2,68 +2,81 @@ var secOffSet = {};
 document.addEventListener("DOMContentLoaded", function (event) {
     setNavOffSets();
 });
-window.onresize = function(event) {
+window.onresize = function (event) {
     setNavOffSets();
 };
 
 window.onscroll = function () {
     var nav = document.getElementById("nav");
     var windowOffSet = window.scrollY;
-   
+
     stickyNav(nav, windowOffSet);
-    activeNavLink(nav,windowOffSet);
+    activeNavLink(nav, windowOffSet);
 };
 
-function setNavOffSets(){
+function setNavOffSets() {
     secOffSet.home = document.getElementById("home").offsetTop;
     secOffSet.about = document.getElementById("about").offsetTop;
     secOffSet.experience = document.getElementById("experience").offsetTop;
     secOffSet.education = document.getElementById("education").offsetTop;
     secOffSet.volunteer = document.getElementById("volunteer").offsetTop;
-    secOffSet.contact = document.getElementById("contact").offsetTop;
 }
 
-function stickyNav(nav, windowOffSet){
-    if (windowOffSet - nav.offsetTop > 0) {
+function stickyNav(nav, windowOffSet) {
+    var stickHeight = document.getElementById('home').offsetHeight - nav.offsetHeight;
+    if (windowOffSet > stickHeight) {
         nav.style.position = "fixed";
         nav.style.top = 0;
+        nav.style.bottom = "auto";
     } else {
         nav.style.position = "absolute";
-        nav.style.top = "100%";
+        nav.style.top = "auto";
+        nav.style.bottom = 0;
     }
 }
 
-function activeNavLink(nav, windowOffSet){
-    for (var sec in secOffSet) {
-        if (windowOffSet >= secOffSet[sec] && (secOffSet[sec] + document.getElementById(sec).offsetHeight) > windowOffSet) {
-            var activeNav = document.getElementsByClassName(sec)[0].getElementsByTagName("span")[0];
-            if (!activeNav.className.includes("active")) {
-                document.getElementsByClassName("active")[0].className = "";
-                activeNav.className = "active";
+function activeNavLink(nav, windowOffSet) {
+    if (window.scrollY + window.innerHeight == document.body.offsetHeight) {
+        document.getElementsByClassName("active-nav")[0].className = "";
+        document.getElementsByClassName('contact')[0].getElementsByTagName("span")[0].className = "active-nav";
+    } else {
+        var navHeight = document.getElementById("nav").offsetHeight;
+        for (var sec in secOffSet) {
+            if (windowOffSet >= secOffSet[sec] - navHeight && (secOffSet[sec] + document.getElementById(sec).offsetHeight) > windowOffSet) {
+                var activeNav = document.getElementsByClassName(sec)[0].getElementsByTagName("span")[0];
+                if (!activeNav.className.includes("active-nav")) {
+                    document.getElementsByClassName("active-nav")[0].className = "";
+                    activeNav.className = "active-nav";
+                }
             }
         }
     }
 }
 
 var scrollTimer;
-function scroll(secSpan){
+
+function scroll(secSpan) {
+
     var secName = secSpan.parentElement;
     var sec = document.getElementById(secName.classList[1]);
     var increment;
-    
-    
-    if(sec.offsetTop-window.scrollY>=0){//scrolling down
-        increment = (sec.offsetTop - window.scrollY)/70; //distance to scroll / arbitrary #
-    }else{//scrolling up
-        increment = (window.scrollY - sec.offsetTop)/70*-1;
+    var navHeight = document.getElementById("nav").offsetHeight;
+
+    if (sec.offsetTop - window.scrollY >= 0) { //scrolling down
+        increment = (sec.offsetTop - window.scrollY) / 70; //distance to scroll / arbitrary #
+    } else { //scrolling up
+        increment = (window.scrollY - sec.offsetTop) / 70 * -1;
     }
-    scrollTimer = setInterval( function() { scrolling(sec, increment); }, 1 );
+    scrollTimer = setInterval(function () {
+        scrolling(sec, increment, navHeight);
+    }, 1);
 
 }
-function scrolling(sec, increment){
+
+function scrolling(sec, increment, navHeight) {
     var windowY = window.scrollY;
-    window.scrollTo(sec.offsetLeft, windowY+increment);
-    if((increment>0 && windowY>=sec.offsetTop) || (increment<0 && windowY<=sec.offsetTop)){
+    window.scrollTo(sec.offsetLeft, windowY + increment);
+    if ((window.scrollY + window.innerHeight == document.body.offsetHeight) || (window.scrollY == 0) || (increment > 0 && windowY >= (sec.offsetTop - navHeight)) || (increment < 0 && windowY <= (sec.offsetTop - navHeight))) {
         window.clearInterval(scrollTimer)
     }
 }
@@ -77,14 +90,11 @@ function toggleNav() {
     }
 }
 
-function displayTab(tab) {
-    var active = tab.parentElement.getElementsByClassName("active");
+function displayTab(tabButton) {
+    var tab = tabButton.parentElement;
+    var active = tab.parentElement.getElementsByClassName("active-tab");
     if (active.length > 0) {
-        console.log(active.className);
-        active[0].className = "tab-content";
+        active[0].className = "tab disabled-tab";
     }
-    var content = tab.getElementsByClassName("tab-content");
-    content[0].className += " active";
+    tab.className = "tab active-tab";
 }
-    
-    
